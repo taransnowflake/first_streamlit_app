4d1f7b8bf8bd64e1ea4f89f487b9ae70629e9174
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-10 19:48:48 +0530

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.title('Breakfast Menu')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale ,Spinach & Rocket Smoothie')
