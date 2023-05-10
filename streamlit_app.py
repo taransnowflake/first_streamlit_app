4f35f450b86502f770c733af9250572ed1c22045
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-10 19:52:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
-streamlit.title('Breakfast Menu')
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale ,Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale ,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
