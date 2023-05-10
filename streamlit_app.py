133cabc5478364c7204db450c8a6a0a8ed14bb22
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-10 19:56:14 +0530

    Update streamlit_app.py
    added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale ,Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
