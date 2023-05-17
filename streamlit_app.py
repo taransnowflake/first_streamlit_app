4f92f20741d4b4a44427566aaacaec2e46479593
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-17 18:41:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit" + "watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
