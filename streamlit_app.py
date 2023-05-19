b03d5645cac73247805faba51b02fa24601221c8
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-19 15:39:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def get_fruityvice_data(this_fruit_choice):
  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
  return fruityvice_normalized
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("View our fruit list - Add yourfavourites")
 def get_fruit_load_list():
  with my_cnx.cursor() as my_cur:
   my_cur.execute("select * from fruit_load_list")
