fe6c02a9fbb5d26d5a9ecb35e491de437703338e
taransnowflake <133127037+taransnowflake@users.noreply.github.com>
2023-05-19 15:15:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 from urllib.error import URLError
-
+import snowflake.connector
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
@@ -23,6 +23,18 @@ def get_fruityvice_data(this_fruit_choice):
  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
  return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
+def get_fruit_load_list():
+ with my_cnx.cursor() as my_cur:
+  my_cur.execute("select * from fruit_load_list")
+   return my_cur.fetchall()
+  
+if streamlit.button('Get fruit load list'):
+ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ my_data_rows = get_fruit_load_list()
+ streamlit.dataframe(my_data_rows)
+ 
+ 
+  
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -31,18 +43,14 @@ try:
   else:
    back_from_function = get_fruityvice_data(fruit_choice)
    streamlit.dataframe(back_from_function)
+  
 
 except URLError as e:
   streamlit.error()
 
 streamlit.stop()
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to have ?')
 streamlit.write('Thanks for adding ', add_my_fruit)
